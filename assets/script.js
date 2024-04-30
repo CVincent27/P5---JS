c9d60407ff021a0f53b5436da5a8caef72d0b531
VincentC27 <cardon.vincent@hotmail.fr>
2024-04-30 12:17:09 +0200

    update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const slides = [
   {
-    image: "/assets/images/slideshow/slide1.jpg",
+    image: "./assets/images/slideshow/slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
